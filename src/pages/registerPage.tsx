f26aed54f284f3d1c17cb640ce6938d1cfea5b42
SharmeenFatima-11 <sharmeen.fatima@webrangesolutions.com>
2025-03-06 12:08:03 +0500

    msg

--- src/pages/registerPage.tsx
+++ src/pages/registerPage.tsx
@@ -11,7 +11,7 @@ const RegisterPage = () => {
     <div className="w-screen min-h-screen md:min-h-5/6 bg-[url('/backgroundMobile.svg')] sm:bg-[url('/background.svg')] bg-no-repeat bg-top bg-cover flex flex-col items-center justify-end px-4 sm:px-20 pb-10 relative before:absolute before:bottom-0 before:left-0 before:w-full before:h-32 before:bg-gradient-to-t before:from-black/40 before:to-transparent">
 
     {/* <div className="w-screen min-h-screen md:min-h-5/6 bg-[url('/background.svg')] bg-no-repeat bg-top bg-cover flex flex-col items-center justify-end px-2 sm:px-20 pb-10"> */}
-      <Image src="/images/logoo.png" alt="logo" width={695} height={157} unoptimized className="w-auto h-auto max-w-full"  />
+      <Image src="/images/logoo.png" alt="logo" width={695} height={157}  />
 
       {/* Heading */}
       <p className="font-montserrat font-semibold text-[20px] sm:text-2xl md:text-3xl lg:text-4xl xl:text-5xl leading-tight tracking-normal text-center uppercase mt-6 sm:mt-8 md:mt-10">
