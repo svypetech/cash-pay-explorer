0eb7c2af4fadce4ba0c93f2af76cdff928b4bf3d
SharmeenFatima-11 <sharmeen.fatima@webrangesolutions.com>
2025-03-17 15:16:42 +0500

    tokens

--- src/app/tokens/page.tsx
+++ src/app/tokens/page.tsx
@@ -2,12 +2,9 @@
 import React, { useEffect, useState } from "react";
 import { useDarkMode } from "../context/DarkModeContext";
 import TokenTable from "../../components/tables/tokenTable";
-import Link from "next/link";
 import Pagination from "@/src/components/pagination/pagination";
 import images from "../../data/images.json"
 import Image from "next/image";
-import { ImageError } from "next/dist/server/image-optimizer";
-
 
 const Page = () => {
   const { darkMode } = useDarkMode(); // Get dark mode state
@@ -40,20 +37,33 @@ const Page = () => {
       <div className={`flex flex-col p-4 sm:p-8 rounded-lg ${showDark ? "bg-darkBg" : "border-black/20 border-1 bg-white"}`}>
         <div className="flex gap-x-20 w-full justify-between items-center">
           <p className="flex-1 font-satoshi text-[20px] md:text-[40px] ">Tokens</p>
-          
-          <div className={` ${showDark ? "bg-darkBg" : "bg-white"} flex items-center w-full gap-x-4 justify-center  border ${showDark? 'border-borderDark':'border-black/20'} rounded-lg`}>
-                <Image src={showDark ? images.searchIconDark : images.searchIconLight} alt="search" width={15} height={15}
-                    className="m-1 sm:m-3 w-3 h-3 sm:w-4 sm:h-4" />
-                <input
-                    type="text"
-                    placeholder="Search..."
-                    value={search}
-                    onChange={(e) => setSearch(e.target.value)}
-                    className={`w-full p-1 border-none outline-none text:[10px] sm:text-[16px] placeholder:text-[10px] sm:placeholder:text-[16px] focus:ring-0 ${showDark ? "placeholder-white" : "placeholder-gray-500"} `}
-                />
 
-            </div>
+          <div className={` hidden ${showDark ? "bg-darkBg" : "bg-white"} flex items-center w-full gap-x-4 justify-center  border ${showDark ? 'border-borderDark' : 'border-black/20'} rounded-lg`}>
+            <Image src={showDark ? images.searchIconDark : images.searchIconLight} alt="search" width={15} height={15}
+              className="m-1 sm:m-3 w-3 h-3 sm:w-4 sm:h-4" />
+            <input
+              type="text"
+              placeholder="Search..."
+              value={search}
+              onChange={(e) => setSearch(e.target.value)}
+              className={`w-full p-1 border-none outline-none text:[10px] sm:text-[16px] placeholder:text-[10px] sm:placeholder:text-[16px] focus:ring-0 ${showDark ? "placeholder-white" : "placeholder-gray-500"} `}
+            />
+
+          </div>
           <Pagination currentPage={currentPage} totalPages={totalPages} onPageChange={setCurrentPage} />
+
+        </div>
+
+        <div className={` sm:hidden ${showDark ? "bg-darkBg" : "bg-white"} flex items-center w-full gap-x-1 justify-center my-4  border ${showDark ? 'border-borderDark' : 'border-black/20'} rounded-lg`}>
+          <Image src={showDark ? images.searchIconDark : images.searchIconLight} alt="search" width={15} height={15}
+            className="m-1 w-4 h-4" />
+          <input
+            type="text"
+            placeholder="Search..."
+            value={search}
+            onChange={(e) => setSearch(e.target.value)}
+            className={`w-full p-1 border-none outline-none text:[10px] sm:text-[16px] placeholder:text-[10px] sm:placeholder:text-[16px] focus:ring-0 ${showDark ? "placeholder-white" : "placeholder-gray-500"} `}
+          />
 
         </div>
         <div className=" my-4">
--- src/components/tables/tokenTable.tsx
+++ src/components/tables/tokenTable.tsx
@@ -36,29 +36,29 @@ const TokenTable: React.FC<TokenProps> = ({ data }) => {
         <div className={`flex-1 rounded-lg shadow-lg w-full`} style={{ boxShadow: "0px 0px 12px 2px rgba(0, 0, 0, 0.06)" }}>
             {/* Table */}
             <div className="shadow-md rounded-lg overflow-hidden w-full">
-                <table className="w-full text-left table-fixed">
+                <table className="w-full text-left table-fixed min-w-30">
                     <thead className="bg-secondary/10">
-                        <tr className="font-satoshi text-[16px]">
-                            <th className="p-4 text-left w-2/6">Token</th>
-                            <th className="p-4 text-left w-3/6">Address</th>
-                            <th className="p-4 text-left w-2/6">Total Supply</th>
-                            <th className="p-4 text-left w-1/6">Holders Count</th>
+                        <tr className="font-satoshi text-[12px] sm:text-[16px] p-2 sm:p-4">
+                            <th className="p-2 sm:p-4 text-left w-1/5 sm:w-2/6">Token</th>
+                            <th className="p-2 sm:p-4 text-left w-1/5 sm:w-3/6">Address</th>
+                            <th className="p-2 sm:p-4 text-left w-1/5 sm:w-2/6">Total Supply</th>
+                            <th className="p-2 sm:p-4 text-left w-1/5 sm:w-1/6">Holders Count</th>
                         </tr>
                     </thead>
                     <tbody>
                         {Array.isArray(data) &&
                             data.map((token, index) => (
-                                <tr key={index} className="border-b">
-                                    <td className={`p-4 font-satoshi text-[16px] ${showDark? "text-secondary": "text-primary"} font-semibold w-2/6 min-w-0 overflow-hidden text-ellipsis whitespace-nowrap`}>
+                                <tr key={index} className="border-b text-[12px] sm:text-[16px]">
+                                    <td className={`p-2 sm:p-4 font-satoshi ${showDark? "text-secondary": "text-primary"} font-semibold w-2/6 min-w-0 break-words`}>
                                         {token.name}
                                     </td>
-                                    <td className={`p-4 font-satoshi text-[16px]  ${showDark? "text-secondary": "text-primary"} w-3/6 min-w-0 overflow-hidden text-ellipsis whitespace-nowrap`}>
+                                    <td className={`p-2 sm:p-4 font-satoshi ${showDark? "text-secondary": "text-primary"} w-3/6 min-w-0 break-words`}>
                                         {token.address}
                                     </td>
-                                    <td className="p-4 font-satoshi text-[16px] w-2/6 min-w-0 overflow-hidden text-ellipsis whitespace-nowrap">
+                                    <td className="p-2 sm:p-4 font-satoshi w-2/6 min-w-0 break-words">
                                         {token.totalSupply}
                                     </td>
-                                    <td className="p-4 font-satoshi text-[16px] w-1/6 min-w-0 text-right">
+                                    <td className="p-2 sm:p-4 font-satoshi w-1/6 min-w-0 text-right">
                                         {token.holderCount}
                                     </td>
                                 </tr>
