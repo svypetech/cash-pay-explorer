d43a039b8250ec198d092c7c6b61964545632ae4
SharmeenFatima-11 <sharmeen.fatima@webrangesolutions.com>
2025-03-18 12:30:39 +0500

    latest changes

--- src/app/apis/page.tsx
+++ src/app/apis/page.tsx
@@ -1,6 +1,13 @@
-import React from 'react'
+'use client'
+import React, { useState } from 'react'
 
 const page = () => {
+  const [queryArray, setQueryArray] = useState([{
+    name: "",
+    url:"",
+    WsUrl: "",
+    headers: []
+  }])
   return (
     <div className="h-screen flex flex-col items-between justify-between">
         page</div>
